78900b342a102570786f7e8925786df2d53cee9d
imisu9 <36559904+imisu9@users.noreply.github.com>
2019-01-13 13:13:36 +0900

    Update drive.py

--- drive.py
+++ drive.py
@@ -110,6 +110,7 @@ def telemetry(sid, data):
         image_array = np.asarray(image)
         # Added line to pre-process image
         #image_array = image_processing(image_array)
+        #image_array = image_array[25:95,:]
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
