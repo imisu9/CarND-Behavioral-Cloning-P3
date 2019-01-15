d1187e22cbb42fb0b6e6ee99399d7de3b2c3861c
imisu9 <36559904+imisu9@users.noreply.github.com>
2019-01-15 10:55:38 +0900

    rollback binary_img()

--- drive.py
+++ drive.py
@@ -50,43 +50,33 @@ import cv2
 import numpy as np
 import sklearn
 
-def image_processing(img, s_thresh=(170,255), sx_thresh=(20,100)):
-    ############################
-    # Image preprocessing
-    # : similar to advanced line detection
-    # : cropping could be done here
-    # 1. cropping
-    # 2. color space conversion to hls
-    # 3. image gradient, sobel x
-    ############################
-    
-    # cropping bottom 25px and top 65px
-    # examined pixs to exclude bonet area on the bottom and non-road area on the top
-    cropped_img = img[25:95,:]
-    # convert to HLS color space and separate the L & S channel
-    hls_img = cv2.cvtColor(cropped_img, cv2.COLOR_RGB2HLS)
-    l_channel = hls_img[:,:,1]
-    s_channel = hls_img[:,:,2]
-    # sobel x
-    #   > take the derivative in x
-    sobelx = cv2.Sobel(l_channel, cv2.CV_64F, 1, 0)
-    #   > absolute x derivatitve to accentuate lines away from horizontal
-    abs_sobelx = np.absolute(sobelx)
-    scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
-    # threshold x gradient
-    sxbinary = np.zeros_like(scaled_sobel)
-    sxbinary[(scaled_sobel >= sx_thresh[0]) & (scaled_sobel <= s_thresh[1])] = 1
-    # threshold color channel
-    s_binary = np.zeros_like(s_channel)
-    s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
-    # > stack each channel to view their individual conttribution in green and blue respectively.
-    # > this returns a stack of the two bianry images, whose components you can see as different colors.
-    #color_binary = np.dstack((np.zeros_like(sxbinary), sxbinary, s_binary)) * 255
-    # combine the two binary thresholds
-    combined_binary = np.zeros_like(sxbinary)
-    combined_binary[(s_binary==1) | (sxbinary==1)] = 1
-    
-    return np.expand_dims(combined_binary, axis=2)
+def binary_img(img, s_thresh=(170,255), sx_thresh=(20,100)):
+  
+  # convert to HLS color space and separate the L & S channel
+  hls_img = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+  l_channel = hls_img[:,:,1]
+  s_channel = hls_img[:,:,2]
+  # sobel x
+  #   > take the derivative in x
+  sobelx = cv2.Sobel(l_channel, cv2.CV_64F, 1, 0)
+  #   > absolute x derivatitve to accentuate lines away from horizontal
+  abs_sobelx = np.absolute(sobelx)
+  scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
+  # threshold x gradient
+  sxbinary = np.zeros_like(scaled_sobel)
+  sxbinary[(scaled_sobel >= sx_thresh[0]) & (scaled_sobel <= s_thresh[1])] = 1
+  # threshold color channel
+  s_binary = np.zeros_like(s_channel)
+  s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
+  # > stack each channel to view their individual conttribution in green and blue respectively.
+  # > this returns a stack of the two bianry images, whose components you can see as different colors.
+  color_binary = np.dstack((np.zeros_like(sxbinary), sxbinary, s_binary)) * 255
+  # combine the two binary thresholds
+  combined_binary = np.zeros_like(sxbinary)
+  combined_binary[(s_binary==1) | (sxbinary==1)] = 1
+
+  #return np.expand_dims(color_binary, axis=2), np.expand_dims(combined_binary, axis=2)
+  return np.expand_dims(combined_binary, axis=2)
 
 # END
 
@@ -109,8 +99,7 @@ def telemetry(sid, data):
         image = Image.open(BytesIO(base64.b64decode(imgString)))
         image_array = np.asarray(image)
         # Added line to pre-process image
-        #image_array = image_processing(image_array)
-        #image_array = image_array[25:95,:]
+        #image_array = binary_img(image_array)
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
